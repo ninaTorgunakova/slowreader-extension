ba84a1fa435ddc3c0c7a3a8a559a3aa2de186519
Nina Torgunakova <ninatorgunakova@gmail.com>
2024-07-27 00:22:48 +0100

    Added timeout for fetch

--- src/background.ts
+++ src/background.ts
@@ -1,15 +1,22 @@
 import { config } from './config';
 
+const FETCH_TIMEOUT_MS = 30000;
+
 chrome.runtime.onConnectExternal.addListener((port) => {
   console.log(port);
   if (port.sender?.origin === config.HOST) {
     console.log(`connection attempt from ${config.HOST}`);
     port.postMessage({ message: 'Extension was connected' });
+
     port.onMessage.addListener(async (message) => {
       console.log(message);
       port.postMessage({ message: 'Message was received' });
       if (message.name === 'fetch' && message.url) {
-        await fetch(message.url, message.options)
+        const options = {
+          ...message.options,
+          signal: AbortSignal.timeout(FETCH_TIMEOUT_MS)
+        }
+        await fetch(message.url, options)
           .then((data) => {
             console.log(data);
             port.postMessage({ data });
